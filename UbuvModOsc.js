9a305a89a099e6dd8393e35e87de2838440b57ae
gag1005 <gag1005@alu.ubu.es>
2022-10-02 19:19:16 +0200

    Rehecho, arreglado muchas cosas

--- UbuvModOsc.js
+++ UbuvModOsc.js
@@ -1,109 +1,137 @@
-document.body.style.backgroundColor = "#1A1A1A";
-document.body.style.color = "#808080";
-document.body.style.border = "red";
+// Colores
+const BACKGROUND_PRIMARY = "#1A1A1A";
+const BACKGROUND_SECONDARY = "#262626";
+const ACCENT = "#333333";
+const CARD = "#262626";
+const NAV = "#262626";
+const NAV_ITEM = "#333333";
+const NAV_BUTTON = "#333333";
+const TEXT_PRIMARY = "#808080";
+const TEXT_SECONDARY = "#666666";
+const CALENDAR_NORMAL = "#262626";
+const CALENDAR_EVENT = "#333333";
+const BOX_BORDER = "#1A1A1A";
 
-for (i of document.getElementsByClassName("activity")){
-    i.style.borderColor = "#1A1A1A";
+
+document.body.style.backgroundColor = BACKGROUND_PRIMARY;
+document.body.style.color = TEXT_PRIMARY;
+document.body.style.border = BOX_BORDER;
+
+// Main body
+for (i of document.getElementsByClassName("mb-3")){
+    i.style.backgroundColor = BACKGROUND_SECONDARY;
 }
 
-for (i of document.getElementsByClassName("section")){
-    i.style.borderColor = "#1A1A1A";
-}
+// Main section
+document.getElementById("region-main").style.backgroundColor = BACKGROUND_SECONDARY;
 
-for (i of document.getElementsByClassName("no-overflow")){
-    i.style.borderColor = "#1A1A1A";
-}
-
-
+// Cards
 for (i of document.getElementsByClassName("card")){
-    i.style.backgroundColor = "#262626";
+    i.style.backgroundColor = BACKGROUND_SECONDARY;
 }
 
-document.getElementById("region-main").style.backgroundColor = "#262626";
+// Header
+for (i of document.getElementsByClassName("page-header")){
+    i.style.backgroundColor = BACKGROUND_SECONDARY;
+}
 
-document.getElementById("nav-drawer").style.backgroundColor = "#262626";
+// Nav
+document.getElementById("nav-drawer").style.backgroundColor = NAV;
+
+// Items del Nav
+for (i of document.getElementsByClassName("list-group-item")){
+    i.style.backgroundColor = NAV_ITEM;
+}
+
+// Texto de los items del Nav
+for (i of document.getElementsByClassName("media")){
+    i.style.color = TEXT_PRIMARY;
+}
+
+// Texto con links
+for (i of document.getElementsByClassName("aalink")){
+    i.style.color = TEXT_SECONDARY;
+}
+
+// TExto con links en las cards
+for (i of document.getElementsByTagName("a")){
+    i.style.color = TEXT_SECONDARY;
+}
+
+// Bordes de las cajas
+for (i of document.getElementsByClassName("border")){
+    i.setAttribute('style', 'border: 1px solid ' + BOX_BORDER + ' !important');
+}
 
 for (i of document.getElementsByClassName("bg-white")){
-    i.setAttribute('style', 'background-color: #262626 !important');
-}
-
-for (i of document.getElementsByClassName("list-group-item list-group-item-action")){
-    i.style.backgroundColor = "#333333";
-}
-
-for (i of document.getElementsByClassName("list-group-item")){
-    i.style.backgroundColor = "#333333";
-}
- 
-for (i of document.getElementsByClassName("bg-gray")){
-    i.setAttribute('style', 'background-color: #333333 !important');
-}
-
-for (i of document.getElementsByClassName("btn-light")){
-    i.style.borderColor = "#1a1a1a"
+    i.setAttribute('style', 'background-color: ' + BACKGROUND_SECONDARY + ' !important');
 }
 
 for (i of document.getElementsByClassName("icon")){
-    i.style.color = "#808080";
+    i.style.color = TEXT_PRIMARY;
 }
 
-for (i of document.getElementsByClassName("media-body")){
-    i.style.color = "#808080";
+for (i of document.getElementsByClassName("bg-gray")){
+    i.setAttribute('style', 'background-color: ' + NAV_BUTTON + ' !important');
 }
 
-for (i of document.getElementsByClassName("font-weight-bold")){
-    i.style.color = "#999999";
-}
-
-for (i of document.getElementsByClassName("nav-link")){
-    i.style.color = "#808080";
-}
-
-for (i of document.getElementsByClassName("site-name")){
-    i.style.color = "#808080";
-}
-
-for (i of document.getElementsByClassName("page-header-headings")){
-    i.style.color = "#808080";
-}
-
-document.getElementById("frontpage-course-list").style.color = "#808080";
-document.getElementById("site-news-forum").style.color = "#808080";
-
-for (i of document.getElementsByClassName("card-title")){
-    i.style.color = "#808080";
-}
-
-for (i of document.getElementsByClassName("aalink")){
-    i.style.color = "#FF8080";
-}
-
-for (i of document.getElementsByClassName("border")){
-    i.setAttribute('style', 'border: 1px solid #1A1A1A !important');
-}
-
-for (i of document.getElementsByClassName("card-text")){
-    i.style.color = "#FF8080";
-}
-
-for (i of document.getElementsByClassName("userbutton")){
-    i.style.color = "#FF8080";
-}
-
-for (i of document.getElementsByClassName("logoutusername")){
-    i.style.color = "#FF8080";
-}
-
-for (i of document.getElementsByClassName("logoutfooter")){
-    i.style.color = "#FF8080";
-}
-
-for (i of document.getElementsByClassName("no-overflow")){
-    i.setAttribute('style', 'color: #FF8080 !important');
+for (i of document.getElementsByClassName("btn-light")){
+    i.style.borderColor = BACKGROUND_SECONDARY;
 }
 
 for (i of document.getElementsByClassName("active")){
-    i.style.backgroundColor = "#404040";
+    i.style.borderColor = BOX_BORDER;
 }
 
+for (i of document.getElementsByTagName("th")){
+    i.setAttribute('style', 'border: 1px solid ' + BOX_BORDER);
+}
 
+for (i of document.getElementsByTagName("tr")){
+    i.setAttribute('style', 'color: ' + TEXT_PRIMARY);
+}
+
+for (i of document.getElementsByTagName("td")){
+    i.setAttribute('style', 'border-top: 1px solid ' + BOX_BORDER);
+}
+
+for (i of document.getElementsByClassName("main")){
+    i.setAttribute('style', 'border: 1px solid ' + BOX_BORDER);
+}
+
+for (i of document.getElementsByClassName("cell")){
+    i.style.backgroundColor = ACCENT;
+}
+
+// Dias del calendario
+for (i of document.getElementsByClassName("day")){
+    i.style.backgroundColor = CALENDAR_NORMAL;
+}
+
+// Dias destacados del calendario
+for (i of document.getElementsByClassName("calendar_event_course")){
+    i.style.backgroundColor = CALENDAR_EVENT;
+}
+
+for (i of document.getElementsByClassName("drawer")){
+    i.style.backgroundColor = CALENDAR_EVENT;
+}
+
+for (i of document.getElementsByClassName("activity")){
+    i.setAttribute('style', 'border-bottom: 1px solid ' + BOX_BORDER);
+}
+
+for (i of document.getElementsByTagName("span")){
+    i.style.backgroundColor = BACKGROUND_SECONDARY;
+    i.style.color = TEXT_PRIMARY;
+}
+
+for (i of document.getElementsByTagName("p")){
+    i.style.backgroundColor = BACKGROUND_SECONDARY;
+    i.style.color = TEXT_PRIMARY;
+}
+
+for (i of document.getElementsByTagName("em")){
+    i.style.backgroundColor = BACKGROUND_SECONDARY;
+    i.style.color = TEXT_PRIMARY;
+}
