4c95fba653f9583ea4ac57c0cd36a665a10edba5
gag1005 <gag1005@alu.ubu.es>
2022-10-02 17:25:01 +0200

    Primera versión

---
+++ UbuvModOsc.js
@@ -0,0 +1,109 @@
+document.body.style.backgroundColor = "#1A1A1A";
+document.body.style.color = "#808080";
+document.body.style.border = "red";
+
+for (i of document.getElementsByClassName("activity")){
+    i.style.borderColor = "#1A1A1A";
+}
+
+for (i of document.getElementsByClassName("section")){
+    i.style.borderColor = "#1A1A1A";
+}
+
+for (i of document.getElementsByClassName("no-overflow")){
+    i.style.borderColor = "#1A1A1A";
+}
+
+
+for (i of document.getElementsByClassName("card")){
+    i.style.backgroundColor = "#262626";
+}
+
+document.getElementById("region-main").style.backgroundColor = "#262626";
+
+document.getElementById("nav-drawer").style.backgroundColor = "#262626";
+
+for (i of document.getElementsByClassName("bg-white")){
+    i.setAttribute('style', 'background-color: #262626 !important');
+}
+
+for (i of document.getElementsByClassName("list-group-item list-group-item-action")){
+    i.style.backgroundColor = "#333333";
+}
+
+for (i of document.getElementsByClassName("list-group-item")){
+    i.style.backgroundColor = "#333333";
+}
+ 
+for (i of document.getElementsByClassName("bg-gray")){
+    i.setAttribute('style', 'background-color: #333333 !important');
+}
+
+for (i of document.getElementsByClassName("btn-light")){
+    i.style.borderColor = "#1a1a1a"
+}
+
+for (i of document.getElementsByClassName("icon")){
+    i.style.color = "#808080";
+}
+
+for (i of document.getElementsByClassName("media-body")){
+    i.style.color = "#808080";
+}
+
+for (i of document.getElementsByClassName("font-weight-bold")){
+    i.style.color = "#999999";
+}
+
+for (i of document.getElementsByClassName("nav-link")){
+    i.style.color = "#808080";
+}
+
+for (i of document.getElementsByClassName("site-name")){
+    i.style.color = "#808080";
+}
+
+for (i of document.getElementsByClassName("page-header-headings")){
+    i.style.color = "#808080";
+}
+
+document.getElementById("frontpage-course-list").style.color = "#808080";
+document.getElementById("site-news-forum").style.color = "#808080";
+
+for (i of document.getElementsByClassName("card-title")){
+    i.style.color = "#808080";
+}
+
+for (i of document.getElementsByClassName("aalink")){
+    i.style.color = "#FF8080";
+}
+
+for (i of document.getElementsByClassName("border")){
+    i.setAttribute('style', 'border: 1px solid #1A1A1A !important');
+}
+
+for (i of document.getElementsByClassName("card-text")){
+    i.style.color = "#FF8080";
+}
+
+for (i of document.getElementsByClassName("userbutton")){
+    i.style.color = "#FF8080";
+}
+
+for (i of document.getElementsByClassName("logoutusername")){
+    i.style.color = "#FF8080";
+}
+
+for (i of document.getElementsByClassName("logoutfooter")){
+    i.style.color = "#FF8080";
+}
+
+for (i of document.getElementsByClassName("no-overflow")){
+    i.setAttribute('style', 'color: #FF8080 !important');
+}
+
+for (i of document.getElementsByClassName("active")){
+    i.style.backgroundColor = "#404040";
+}
+
+
